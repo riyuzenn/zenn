fd13c0fc6e8c189717786144946bd2bac1179cda
riyuzenn <riyuzenn@gmail.com>
2022-07-20 15:47:56 +0200

    upt: add post

--- zenn.config.js
+++ zenn.config.js
@@ -8,5 +8,11 @@ module.exports = {
             date: "06/02/2022",
             slug: "test",
         },
+       {
+          title: "my desktop configuration",
+          desc: "all of my configs to personalize my own system",
+          date: "07/20/2022",
+          slug: "dotfiles"
+       }
     ]
 }
